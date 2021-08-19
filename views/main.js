53a96a799e962ed16e84a22460c7f62a97d82dc6
Tom Fishwick <fishwickt@gmail.com>
2021-08-19 20:26:00 +0100

    Got client to show the results

--- views/main.js
+++ views/main.js
@@ -1,9 +1,9 @@
 let img;
 let fr;
 
+const results = ['Cat', 'Dog'];
+
 function loadImage() {
-
-
     let uploader = document.getElementById("imgupload");
     if (!uploader.files[0]) {
         alert("No file selected");
@@ -28,30 +28,18 @@ function imageLoaded() {
     canvas.width = 150;
     canvas.height = 150;
     context.drawImage(img, 0,0, 150, 150);
-    
-    //console.log(canvas.toDataURL("image/png"));
-    //let tensor = tf.browser.fromPixels(canvas);
-    //tensor.shape = [null, 150, 150, 3];
-    //console.log("tensor", tensor);
-    //console.log("tensor", tensor.toString());
-    /*fetch('/predict', {
-        method: 'POST',
-        body: JSON.stringify({tensor: [tensor]}),
-    })
-    .then(res => {
-        console.log("prediction", res);
-    })
-    .catch(e => {
-        console.error("prediction", e);
-    });*/
-    //console.log(context.getImageData(0,0,150,150));
 
     $.post('/predict', 
     {
-        //'tensor': JSON.stringify(tensor.toString())
         'pic': canvas.toDataURL("image/png").split(';base64,')[1]
     }, function(res) {
         console.log('predict', res);
         console.log('prediction', res['prediction']);
-    })
+        if (res['prediction'] == -1) {
+            $('#res').text("Error");
+        }
+        else {
+            $('#res').text(results[res['prediction']]);
+        }
+    });
 }
