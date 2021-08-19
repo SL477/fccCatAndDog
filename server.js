cc65859720f94332e4104310951f8341cf111845
Tom Fishwick <fishwickt@gmail.com>
2021-08-19 19:42:51 +0100

    Got server to work out whether image is cat or dog
    Need to send data back to the client

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 //const { async } = require('regenerator-runtime');
 const path = require("path");
+const bodyParser = require('body-parser');
+const tfjs = require('@tensorflow/tfjs');
 
 //const model = tf.loadLayersModel('file:\\\C:\src\fccCatAndDog\jsmodel\model.json');
 
 const app = express();
+
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.route('/')
     .get(function (req, res) {
@@ -22,18 +29,39 @@ app.route('/tf.min.js')
         res.sendFile(process.cwd() + "/views/tf.min.js");
     });
 
-async function predict() {
+async function predict(pic) {
     const handler = tf.io.fileSystem('./jsmodel/model.json');
     const model = await tf.loadLayersModel(handler);
-    //model.predict())
-    let data = tf.FromPixels()
-    return model.summary();
+    //let ex = tfjs.browser.fromPixels(pic);
+    const b = Buffer.from(pic, 'base64');
+    let ex = tf.node.decodeImage(b, 3);
+    //ex.shape = [null, 150, 150, 3];
+    ex = ex.expandDims(0)
+    //ex = ex.reshape([null, 150, 150, 3]);
+    //console.log("shape", ex.shape);
+    let p = model.predict(ex);
+    //console.log("pred",p);
+    console.log("pred",p.dataSync()[0]);
+    //console.log("pred",p.toString());
+    return p.dataSync()[0];
 }
 
 app.route('/predict')
-    .get(function(req, res) {
-        res.json(predict());
+    .post(function(req, res) {
+        //console.log('body', req.body);
+        res.json({'prediction': predict(req.body.pic)});
     });
+
+async function summary() {
+    const handler = tf.io.fileSystem('./jsmodel/model.json');
+    const model = await tf.loadLayersModel(handler);
+    model.summary();
+}
+
+app.route('/summary').get(function(req,res) {
+    summary();
+    res.send("check server");
+});
 
 app.use(
     '/jsmodel/model',
--- views/main.js
+++ views/main.js
@@ -35,8 +35,30 @@ function imageLoaded() {
     canvas.width = 150;
     canvas.height = 150;
     context.drawImage(img, 0,0, 150, 150);
-
+    
     //console.log(canvas.toDataURL("image/png"));
-    let tensor = tf.browser.fromPixels(canvas);
-    console.log("tensor", tensor);
+    //let tensor = tf.browser.fromPixels(canvas);
+    //tensor.shape = [null, 150, 150, 3];
+    //console.log("tensor", tensor);
+    //console.log("tensor", tensor.toString());
+    /*fetch('/predict', {
+        method: 'POST',
+        body: JSON.stringify({tensor: [tensor]}),
+    })
+    .then(res => {
+        console.log("prediction", res);
+    })
+    .catch(e => {
+        console.error("prediction", e);
+    });*/
+    //console.log(context.getImageData(0,0,150,150));
+
+    $.post('/predict', 
+    {
+        //'tensor': JSON.stringify(tensor.toString())
+        'pic': canvas.toDataURL("image/png").split(';base64,')[1]
+    }, function(res) {
+        console.log('predict', res);
+        console.log('prediction', res['prediction']);
+    })
 }
